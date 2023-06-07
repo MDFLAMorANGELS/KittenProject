8f9a9250daa38fa506a557731d34479ba7ad9fa6
ChloeLevieil <chloe.levieil@outlook.fr>
2023-06-07 12:45:17 +0200

    fix user sign_up

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
   has_many :orders
 
-  belongs_to :cart
+  has_one :cart
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
